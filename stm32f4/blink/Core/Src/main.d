ebf98b308e8438622f08ee5960d2222d403e6ebf
lugarriza <leonardo.ugarriza@gmail.com>
2021-03-07 00:28:26 -0500

    Initial Commit

---
+++ stm32f4/blink/Core/Src/main.d
@@ -0,0 +1,104 @@
+/mnt/d/STM32/Projects/Blink/Core/Src/main: \
+ /mnt/d/STM32/Projects/Blink/Core/Src/main.c \
+ /mnt/d/STM32/Projects/Blink/Core/Inc/main.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /mnt/d/STM32/Projects/Blink/Core/Inc/stm32f4xx_hal_conf.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Include/core_cm4.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Include/cmsis_version.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Include/mpu_armv7.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ /mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ /mnt/d/STM32/Projects/Blink/USB_HOST/App/usb_host.h
+
+/mnt/d/STM32/Projects/Blink/Core/Inc/main.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/mnt/d/STM32/Projects/Blink/Core/Inc/stm32f4xx_hal_conf.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Include/core_cm4.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Include/cmsis_version.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Include/mpu_armv7.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+/mnt/d/STM32/Projects/Blink/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+/mnt/d/STM32/Projects/Blink/USB_HOST/App/usb_host.h:
